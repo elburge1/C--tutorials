366973fa1d894cfaed8df006fc85e5001f9579b1
Edward Burgess <elburgess1@gmail.com>
2016-08-16 13:51:48 -0700

    tutorial 2

--- FirstConsoleProjectSolution/FirstConsoleProject/Program.cs
+++ FirstConsoleProjectSolution/FirstConsoleProject/Program.cs
@@ -4,9 +4,9 @@ namespace FirstConsoleProject
 {
 	class MainClass
 	{
-		public static void Main(string[] args)
+		public static void Main(string[] args) //this is a method called 'Main'. It is called when the program starts
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("Hello, I'm a computer. I'm sent from the future.");
 		}
 	}
 }
