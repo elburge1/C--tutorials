c3980dd5edc2da916dd78a7d9e746992808a6681
Edward Burgess <elburgess1@gmail.com>
2016-08-16 14:37:46 -0700

    improve divider calc

--- FirstConsoleProjectSolution/FirstConsoleProject/Program.cs
+++ FirstConsoleProjectSolution/FirstConsoleProject/Program.cs
@@ -6,13 +6,13 @@ namespace FirstConsoleProject
 	{
 		public static void Main(string[] args) //this is a method called 'Main'. It is called when the program starts
 		{
-			int num01;
-			int num02;
+			double num01;
+			double num02;
 
-			Console.Write("Type a number to be multiplied");
-			num01 = Convert.ToInt32 (Console.ReadLine());
-			Console.Write("Type another number");
-			num02 = Convert.ToInt32(Console.ReadLine());
+			Console.Write("Type a number to be divided ");
+			num01 = Convert.ToDouble (Console.ReadLine());
+			Console.Write("Type another number ");
+			num02 = Convert.ToDouble(Console.ReadLine());
 			Console.WriteLine(num01 + " divided by " + num02 + " gives the result " + num01 / num02);
 			Console.ReadKey();
 		}
