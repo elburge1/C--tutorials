bbccc3bdb92da70842f7399df6af0d4459ae2c2f
Edward Burgess <elburgess1@gmail.com>
2016-08-16 13:55:04 -0700

    add new line

--- FirstConsoleProjectSolution/FirstConsoleProject/Program.cs
+++ FirstConsoleProjectSolution/FirstConsoleProject/Program.cs
@@ -6,7 +6,8 @@ namespace FirstConsoleProject
 	{
 		public static void Main(string[] args) //this is a method called 'Main'. It is called when the program starts
 		{
-			Console.WriteLine("Hello, I'm a computer. I'm sent from the future.");
+			Console.WriteLine("Hello, I'm a computer. I'm sent from the future.\nThis is a new line.");
+			Console.ReadKey();
 		}
 	}
 }
