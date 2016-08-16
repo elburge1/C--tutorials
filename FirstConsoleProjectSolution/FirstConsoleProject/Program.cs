84e299a93cc797686e213d574daf8b2b4606e976
Edward Burgess <elburgess1@gmail.com>
2016-08-16 15:34:40 -0700

    switch statement practice

--- FirstConsoleProjectSolution/FirstConsoleProject/Program.cs
+++ FirstConsoleProjectSolution/FirstConsoleProject/Program.cs
@@ -6,8 +6,11 @@ namespace FirstConsoleProject
 	{
 		public static void Main(string[] args) //this is a method called 'Main'. It is called when the program starts
 		{
-			int num01 = 5;
-			int num02 = 6;
+			Random numbergenerator = new Random();
+
+			int num01 = numbergenerator.Next(1, 11);
+			int num02 = numbergenerator.Next(1, 11);
+
 			Console.Write("What is " + num01 + " plus " + num02 + "? ");
 			int answer = Convert.ToInt32(Console.ReadLine());
 
@@ -16,7 +19,21 @@ namespace FirstConsoleProject
 				Console.WriteLine("That's correct. Good job!");
 			}
 			else {
-				Console.WriteLine("WRONG DUMMY!");
+				int responseindex = numbergenerator.Next(1, 4);
+
+				switch (responseindex)
+				{
+					case 1:
+						Console.WriteLine("WRONG DUMMY!");
+						break;
+					case 2:
+						Console.WriteLine("You tried");
+						break;
+					default:
+						Console.WriteLine("You can do better than that!");
+						break;
+				}
+
 			}
 		}
 	}
