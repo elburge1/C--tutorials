0c018c9db369450f1f4a0132da3e0c189ab2a8e3
Edward Burgess <elburgess1@gmail.com>
2016-08-16 14:27:19 -0700

    make a basic calculator

--- FirstConsoleProjectSolution/FirstConsoleProject/Program.cs
+++ FirstConsoleProjectSolution/FirstConsoleProject/Program.cs
@@ -6,14 +6,14 @@ namespace FirstConsoleProject
 	{
 		public static void Main(string[] args) //this is a method called 'Main'. It is called when the program starts
 		{
-			Console.WriteLine("Hello, I'm a computer. I'm sent from the future.\nThis is a new line.");
-			Console.ReadKey();
-			Console.WriteLine("My name is Eddie");
-			Console.ReadKey();
-			Console.WriteLine("I am 27");
-			Console.ReadKey();
-			Console.WriteLine("My favorite color is blue!");
-			Console.ReadKey();
+			int num01;
+			int num02;
+
+			Console.Write("Type a number to be multiplied");
+			num01 = Convert.ToInt32 (Console.ReadLine());
+			Console.Write("Type another number");
+			num02 = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("The result is " + num01 * num02);
 		}
 	}
 }
