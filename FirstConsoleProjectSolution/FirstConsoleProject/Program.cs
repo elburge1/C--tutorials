62c88675e925732cdba86fb160c94bace313d951
Edward Burgess <elburgess1@gmail.com>
2016-08-16 15:19:31 -0700

    tutorial 3 challenge completed

--- FirstConsoleProjectSolution/FirstConsoleProject/Program.cs
+++ FirstConsoleProjectSolution/FirstConsoleProject/Program.cs
@@ -6,20 +6,18 @@ namespace FirstConsoleProject
 	{
 		public static void Main(string[] args) //this is a method called 'Main'. It is called when the program starts
 		{
-			Console.WriteLine("How old are you?");
-			int age = Convert.ToInt32(Console.ReadLine());
-			if (age < 18)
+			int num01 = 5;
+			int num02 = 6;
+			Console.Write("What is " + num01 + " plus " + num02 + "? ");
+			int answer = Convert.ToInt32(Console.ReadLine());
+
+			if (answer == num01 + num02)
 			{
-				Console.WriteLine("That's too bad. You will have to wait a couple of years.");
-			}
-			else if (age == 18)
-			{
-				Console.WriteLine("Phew! Barely made it!");
+				Console.WriteLine("That's correct. Good job!");
 			}
 			else {
-				Console.WriteLine("You are allowed to enter!");
+				Console.WriteLine("WRONG DUMMY!");
 			}
-			Console.ReadKey();
 		}
 	}
 }
