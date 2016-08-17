30dc0f59e643ba61266ac494b7f06254ddc8b33c
Edward Burgess <elburgess1@gmail.com>
2016-08-17 12:35:27 -0700

    practice with for loops

--- FirstConsoleProjectSolution/FirstConsoleProject/Program.cs
+++ FirstConsoleProjectSolution/FirstConsoleProject/Program.cs
@@ -6,35 +6,11 @@ namespace FirstConsoleProject
 	{
 		public static void Main(string[] args) //this is a method called 'Main'. It is called when the program starts
 		{
-			Random numbergenerator = new Random();
-
-			int num01 = numbergenerator.Next(1, 11);
-			int num02 = numbergenerator.Next(1, 11);
-
-			Console.Write("What is " + num01 + " plus " + num02 + "? ");
-			int answer = Convert.ToInt32(Console.ReadLine());
-
-			if (answer == num01 + num02)
+			for (int i = 1; i <= 10; i++)
 			{
-				Console.WriteLine("That's correct. Good job!");
+				Console.WriteLine(i);
 			}
-			else {
-				int responseindex = numbergenerator.Next(1, 4);
-
-				switch (responseindex)
-				{
-					case 1:
-						Console.WriteLine("WRONG DUMMY!");
-						break;
-					case 2:
-						Console.WriteLine("You tried");
-						break;
-					default:
-						Console.WriteLine("You can do better than that!");
-						break;
-				}
-
-			}
+			Console.ReadKey();
 		}
 	}
 }
