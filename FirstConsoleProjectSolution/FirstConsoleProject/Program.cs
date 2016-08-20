8d58ccf7bc62371ab94f7039a9e107e49a2e582b
Edward Burgess <elburgess1@gmail.com>
2016-08-20 14:30:16 -0700

    dice rolling loop

--- FirstConsoleProjectSolution/FirstConsoleProject/Program.cs
+++ FirstConsoleProjectSolution/FirstConsoleProject/Program.cs
@@ -6,12 +6,20 @@ namespace FirstConsoleProject
 	{
 		public static void Main(string[] args) //this is a method called 'Main'. It is called when the program starts
 		{
-			int i = 1;
-			while (i <= 10)
+			Random numberGen = new Random();
+
+			int numberOfAttempts = 0;
+			int attempt = 0;
+
+			while (attempt != 6)
 			{
-				Console.WriteLine(i);
-				i++;
+				attempt = numberGen.Next(1, 7);
+				Console.WriteLine("Tom rolled: " + attempt + ".");
+				numberOfAttempts++;
 			}
+
+			Console.WriteLine("It took tom " + numberOfAttempts + " attempts to roll a 6.");
+
 			Console.ReadKey();
 		}
 	}
