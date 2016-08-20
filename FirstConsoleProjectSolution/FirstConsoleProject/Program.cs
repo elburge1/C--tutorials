c1d44b8e53ec45625eca299f73fedb94329b3e89
Edward Burgess <elburgess1@gmail.com>
2016-08-20 14:41:40 -0700

    lesson on methods

--- FirstConsoleProjectSolution/FirstConsoleProject/Program.cs
+++ FirstConsoleProjectSolution/FirstConsoleProject/Program.cs
@@ -6,21 +6,14 @@ namespace FirstConsoleProject
 	{
 		public static void Main(string[] args) //this is a method called 'Main'. It is called when the program starts
 		{
-			Random numberGen = new Random();
-
-			int numberOfAttempts = 0;
-			int attempt = 0;
-
-			while (attempt != 6)
-			{
-				attempt = numberGen.Next(1, 7);
-				Console.WriteLine("Tom rolled: " + attempt + ".");
-				numberOfAttempts++;
-			}
-
-			Console.WriteLine("It took tom " + numberOfAttempts + " attempts to roll a 6.");
+			GreetUser();
 
 			Console.ReadKey();
+		}
+
+		public static void GreetUser()
+		{
+			Console.WriteLine("Hello world!");
 		}
 	}
 }
