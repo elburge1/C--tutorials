bbf2645cf74c3f9c422df06c4f65c75662c7a59a
Edward Burgess <elburgess1@gmail.com>
2016-08-24 14:48:47 -0700

    more fun with arrays

--- FirstConsoleProjectSolution/FirstConsoleProject/Program.cs
+++ FirstConsoleProjectSolution/FirstConsoleProject/Program.cs
@@ -15,17 +15,17 @@ namespace FirstConsoleProject
 
 		public static void createGrid()
 		{
-			int width = 5;
-			int height = 5;
+			int width = 20;
+			int height = 20;
 
 			int[,] grid = new int[width, height];
-			grid[2, 3] = 5;
 
 			for (int x = 0; x < width; x++)
 			{
 				for (int y = 0; y < height; y++)
 				{
-					Console.Write(grid[x, y] + ", ");
+					grid[x, y] = x + y;
+					Console.Write(grid[x, y]);
 				}
 				Console.WriteLine();
 			}
