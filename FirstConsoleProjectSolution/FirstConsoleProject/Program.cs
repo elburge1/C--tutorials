98fb5d68bd3acdec1da2b1efbc2a4f771ed1a8bf
Edward Burgess <elburgess1@gmail.com>
2016-08-24 14:43:21 -0700

    multidimensional arrays

--- FirstConsoleProjectSolution/FirstConsoleProject/Program.cs
+++ FirstConsoleProjectSolution/FirstConsoleProject/Program.cs
@@ -7,20 +7,28 @@ namespace FirstConsoleProject
 	{
 		public static void Main(string[] args) //this is a method called 'Main'. It is called when the program starts
 		{
-			List<int> numbers = new List<int>();
-
-			numbers.Add(13);
-			numbers.Add(4);
-			numbers.Add(25);
-
-			numbers.Remove(4);
-
-			for (int i = 0; i < numbers.Count; i++)
-			{
-				Console.WriteLine(numbers[i]);
-			}
+			//Multi-dimensional arrays
+			createGrid();
 
 			Console.ReadKey();
+		}
+
+		public static void createGrid()
+		{
+			int width = 5;
+			int height = 5;
+
+			int[,] grid = new int[width, height];
+			grid[2, 3] = 5;
+
+			for (int x = 0; x < width; x++)
+			{
+				for (int y = 0; y < height; y++)
+				{
+					Console.Write(grid[x, y] + ", ");
+				}
+				Console.WriteLine();
+			}
 		}
 	}
 }
