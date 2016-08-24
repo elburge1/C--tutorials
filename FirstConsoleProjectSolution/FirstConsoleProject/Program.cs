42b1ee1b2a2e76621df30c52682fc755e45c1e9e
Edward Burgess <elburgess1@gmail.com>
2016-08-24 14:28:09 -0700

    work with lists

--- FirstConsoleProjectSolution/FirstConsoleProject/Program.cs
+++ FirstConsoleProjectSolution/FirstConsoleProject/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FirstConsoleProject
 {
@@ -6,15 +7,13 @@ namespace FirstConsoleProject
 	{
 		public static void Main(string[] args) //this is a method called 'Main'. It is called when the program starts
 		{
-			string[] names = new string[3]
-			{
-				"Tom", "Mark", "Matt"
-			};
+			List<int> numbers = new List<int>();
 
-			foreach (string name in names)
-			{
-				Console.WriteLine(name);
-			}
+			numbers.Add(13);
+			numbers.Add(4);
+			numbers.Add(25);
+
+			Console.WriteLine(numbers[2]);
 
 			Console.ReadKey();
 		}
