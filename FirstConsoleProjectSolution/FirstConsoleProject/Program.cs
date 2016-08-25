dc70b7a7a3ee9a0836ad969882a3752950e87e0f
Edward Burgess <elburgess1@gmail.com>
2016-08-25 14:33:10 -0700

    more messing with classes

--- FirstConsoleProjectSolution/FirstConsoleProject/Program.cs
+++ FirstConsoleProjectSolution/FirstConsoleProject/Program.cs
@@ -5,10 +5,28 @@ namespace FirstConsoleProject
 {
 	class Animal
 	{
-		public string name = "Spotty";
-		public int age = 6;
-		public float happiness = 0.5f;
 
+		//class variables
+		public static int count = 0;
+
+		public string name;
+		public int age;
+		public float happiness;
+
+
+		//class constructors
+
+		public Animal(string _name, int _age, float _happiness)
+		{
+			name = _name;
+			age = _age;
+			happiness = _happiness;
+
+			count++;
+		}
+
+
+		//class methods
 		public void Print()
 		{
 			Console.WriteLine("name: " + name);
@@ -22,8 +40,13 @@ namespace FirstConsoleProject
 		public static void Main(string[] args) //this is a method called 'Main'. It is called when the program starts
 		{
 			//classes
-			Animal dog = new Animal();
+			Animal dog = new Animal("Spotty", 6, 0.5f);
 			dog.Print();
+
+			Animal cat = new Animal("Mr. Beans", 3, 0.9f);
+			cat.Print();
+
+			Console.WriteLine("Number of animals: " + Animal.count);
 
 			Console.ReadKey();
 		}
