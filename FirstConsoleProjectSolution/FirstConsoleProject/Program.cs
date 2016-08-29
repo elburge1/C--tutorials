dbd6e9ec329ed06c91397b42d9b31c5dfaabd41b
Edward Burgess <elburgess1@gmail.com>
2016-08-29 12:33:31 -0700

    practice with inheritance

--- FirstConsoleProjectSolution/FirstConsoleProject/Program.cs
+++ FirstConsoleProjectSolution/FirstConsoleProject/Program.cs
@@ -3,50 +3,51 @@ using System.Collections.Generic;
 
 namespace FirstConsoleProject
 {
-	class Animal
-	{
-
-		//class variables
-		public static int count = 0;
-
-		public string name;
-		public int age;
-		public float happiness;
-
-
-		//class constructors
-
-		public Animal(string _name, int _age, float _happiness)
-		{
-			name = _name;
-			age = _age;
-			happiness = _happiness;
-
-			count++;
-		}
-
-
-		//class methods
-		public void Print()
-		{
-			Console.WriteLine("name: " + name);
-			Console.WriteLine("age: " + age);
-			Console.WriteLine("happiness: " + happiness);
-		}
-	}
-
 	class MainClass
 	{
+		class Animal
+		{
+			public string name;
+			public int age;
+			public float happiness;
+
+			public void PrintBase()
+			{
+				Console.WriteLine("name: " + name);
+				Console.WriteLine("age: " + age);
+				Console.WriteLine("happiness: " + happiness);
+			}
+		}
+
+		class Dog : Animal
+		{
+			public int spotCount;
+
+			public void Bark()
+			{
+				Console.WriteLine("Woof");
+			}
+		}
+
+		class Cat : Animal
+		{
+			public float cuteness;
+
+			public void Meow()
+			{
+				Console.WriteLine("Meow");
+			}
+		}
+
 		public static void Main(string[] args) //this is a method called 'Main'. It is called when the program starts
 		{
-			//classes
-			Animal dog = new Animal("Spotty", 6, 0.5f);
-			dog.Print();
-
-			Animal cat = new Animal("Mr. Beans", 3, 0.9f);
-			cat.Print();
-
-			Console.WriteLine("Number of animals: " + Animal.count);
+			Dog spotty = new Dog();
+			spotty.name = "Spotty";
+			spotty.age = 4;
+			spotty.happiness = 0.8f;
+			spotty.spotCount = 25;
+			spotty.PrintBase();
+			spotty.Bark();
 
 			Console.ReadKey();
 		}
