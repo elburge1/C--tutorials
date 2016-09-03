3096591b8ea6f3129577ca155b7e0b67cba95c34
Edward Burgess <elburgess1@gmail.com>
2016-09-02 18:27:13 -0700

    work with enums!

--- FirstConsoleProjectSolution/FirstConsoleProject/Program.cs
+++ FirstConsoleProjectSolution/FirstConsoleProject/Program.cs
@@ -10,55 +10,21 @@ namespace FirstConsoleProject
 			public string name;
 			public int age;
 			public float happiness;
-
-			public void PrintBase()
-			{
-				Console.WriteLine("name: " + name);
-				Console.WriteLine("age: " + age);
-				Console.WriteLine("happiness: " + happiness);
-			}
 		}
 
 		class Dog : Animal
 		{
-			public int spotCount;
 
-			public void Bark()
+
+			public void Print()
 			{
-				Console.WriteLine("Woof");
-				base.happiness += 0.1f;
+				Console.WriteLine("Name: " + name);
+				Console.WriteLine("Age: " + age);
+				Console.WriteLine("Happiness: " + happiness);
 			}
 		}
-
-		class Cat : Animal
-		{
-			public float cuteness;
-
-			public void Meow()
-			{
-				Console.WriteLine("Meow");
-			}
-		}
-
 		public static void Main(string[] args) //this is a method called 'Main'. It is called when the program starts
 		{
-			Dog spotty = new Dog();
-			spotty.name = "Spotty";
-			spotty.age = 4;
-			spotty.happiness = 0.8f;
-			spotty.spotCount = 25;
-			spotty.PrintBase();
-			spotty.Bark();
-			spotty.PrintBase();
-
-			Cat heisenberg = new Cat();
-			heisenberg.name = "Heisenberg";
-			heisenberg.age = 13;
-			heisenberg.happiness = 0.3f;
-			heisenberg.cuteness = 0.4f;
-			heisenberg.PrintBase();
-			heisenberg.Meow();
-
 			Console.ReadKey();
 		}
 	}
