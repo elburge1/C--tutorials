4bfaebc35fab9fab8cc2fa642dfdae7609c5ec2a
Edward Burgess <elburgess1@gmail.com>
2016-09-02 18:40:07 -0700

    more work with enums

--- FirstConsoleProjectSolution/FirstConsoleProject/Program.cs
+++ FirstConsoleProjectSolution/FirstConsoleProject/Program.cs
@@ -5,6 +5,7 @@ namespace FirstConsoleProject
 {
 	class MainClass
 	{
+
 		class Animal
 		{
 			public string name;
@@ -14,17 +15,30 @@ namespace FirstConsoleProject
 
 		class Dog : Animal
 		{
+			public string breed;
 
+			public Dog(string _name, int _age, float _happiness, string _breed)
+			{
+				name = _name;
+				age = _age;
+				happiness = _happiness;
+				breed = _breed;
+			}
 
 			public void Print()
 			{
 				Console.WriteLine("Name: " + name);
 				Console.WriteLine("Age: " + age);
 				Console.WriteLine("Happiness: " + happiness);
+
+				Console.WriteLine("Breed: " + breed);
 			}
 		}
 		public static void Main(string[] args) //this is a method called 'Main'. It is called when the program starts
 		{
+			Dog hulk = new Dog("Hulk", 2, 0.7f, "Boxer");
+			hulk.Print();
+
 			Console.ReadKey();
 		}
 	}
