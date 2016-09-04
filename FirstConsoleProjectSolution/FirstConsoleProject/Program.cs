1894202ecf19d46436325bf0f6b59d883ddec6cd
Edward Burgess <elburgess1@gmail.com>
2016-09-03 23:19:33 -0700

    creating a player to work with get/set

--- FirstConsoleProjectSolution/FirstConsoleProject/Program.cs
+++ FirstConsoleProjectSolution/FirstConsoleProject/Program.cs
@@ -5,10 +5,17 @@ namespace FirstConsoleProject
 {
 	class MainClass
 	{
+		class Player
+		{
+			public int health = 100;
+		}
+
 		public static void Main(string[] args) //this is a method called 'Main'. It is called when the program starts
 		{
-			DateTime curTime = DateTime.Now;
-			Console.WriteLine(curTime);
+			Player tom = new Player();
+			Console.WriteLine(tom.health);
+			tom.health = 40;
+			Console.WriteLine(tom.health);
 
 			Console.ReadKey();
 		}
