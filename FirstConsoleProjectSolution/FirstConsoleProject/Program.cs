81a1140a5d8bc1993bddffc8de41bab19f8c0b8c
Edward Burgess <elburgess1@gmail.com>
2016-09-03 23:21:52 -0700

    adding methods to player class

--- FirstConsoleProjectSolution/FirstConsoleProject/Program.cs
+++ FirstConsoleProjectSolution/FirstConsoleProject/Program.cs
@@ -8,13 +8,17 @@ namespace FirstConsoleProject
 		class Player
 		{
 			public int health = 100;
+
+			public void Damage(int _dmg)
+			{
+				health -= _dmg;
+			}
 		}
 
 		public static void Main(string[] args) //this is a method called 'Main'. It is called when the program starts
 		{
 			Player tom = new Player();
-			Console.WriteLine(tom.health);
-			tom.health = 40;
+			tom.Damage(10);
 			Console.WriteLine(tom.health);
 
 			Console.ReadKey();
