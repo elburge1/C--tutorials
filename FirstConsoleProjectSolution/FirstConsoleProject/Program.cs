4b5ca759cb1f204d66625be7d1d177223cf39e25
Edward Burgess <elburgess1@gmail.com>
2016-09-03 23:13:17 -0700

    clean space to work with properties

--- FirstConsoleProjectSolution/FirstConsoleProject/Program.cs
+++ FirstConsoleProjectSolution/FirstConsoleProject/Program.cs
@@ -5,41 +5,8 @@ namespace FirstConsoleProject
 {
 	class MainClass
 	{
-		enum Breed { Bulldog, Boxer, Chihuahua, Shepherd };
-
-		class Animal
-		{
-			public string name;
-			public int age;
-			public float happiness;
-		}
-
-		class Dog : Animal
-		{
-			public Breed breed;
-
-			public Dog(string _name, int _age, float _happiness, Breed _breed)
-			{
-				name = _name;
-				age = _age;
-				happiness = _happiness;
-				breed = _breed;
-			}
-
-			public void Print()
-			{
-				Console.WriteLine("Name: " + name);
-				Console.WriteLine("Age: " + age);
-				Console.WriteLine("Happiness: " + happiness);
-
-				Console.WriteLine("Breed: " + breed);
-			}
-		}
 		public static void Main(string[] args) //this is a method called 'Main'. It is called when the program starts
 		{
-			Dog hulk = new Dog("Hulk", 2, 0.7f, Breed.Boxer);
-			hulk.Print();
-
 			Console.ReadKey();
 		}
 	}
