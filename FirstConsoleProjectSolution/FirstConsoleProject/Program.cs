3bdb70ef60579d3761a1a673d521d1f1854bba34
Edward Burgess <elburgess1@gmail.com>
2016-09-06 15:17:36 -0700

    learning about interfaces

--- FirstConsoleProjectSolution/FirstConsoleProject/Program.cs
+++ FirstConsoleProjectSolution/FirstConsoleProject/Program.cs
@@ -5,39 +5,30 @@ namespace FirstConsoleProject
 {
 	class MainClass
 	{
-		class Player
+		interface IItem
 		{
-			private int _health = 100;
-			public int health 
-			{ 
-				get
-				{
-					return _health;
-				}
-				set
-				{
-					if (value <= 0)
-					{
-						_health = 0;
-					}
-					else if (value >= 100)
-					{
-						_health = 100;
-					}
-					else
-					{
-						_health = value;
-					}
-				}
+			string name { get; set; }
+			int goldValue { get; set; }
+
+			void Equip();
+			void Sell();
+		}
+
+		class Sword
+		{
+			public string name { get; set;}
+			public int goldValue { get; set;}
+
+			public Sword(string _name)
+			{
+				name = _name;
+				goldValue = 100;
 			}
 		}
 
 		public static void Main(string[] args) //this is a method called 'Main'. It is called when the program starts
 		{
-			Player tom = new Player();
-			Console.WriteLine(tom.health);
-			tom.health -= 200;
-			Console.WriteLine(tom.health);
+
 
 			Console.ReadKey();
 		}
